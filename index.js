711101368b2b4851b659f4c31a35d65f4417b700
stevevillardi <stevevillardi@gmail.com>
2019-11-20 09:42:31 -0500

    update psudocode

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const Word = require('./word.js')
 
+//install prompt or inquirer
+//look for random word genreator package
+//loop through selected word and create letter object for each char\
+//pass letter array to new word object
+//game logic
